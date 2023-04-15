1c61e25462b0146baed8019f2da734c136f60007
Saverio Tufano <saverio.tufano@gmail.com>
2023-04-15 16:02:31 +0100

    Performs the following project tasks: Task1, Task2, Task3

--- milestone_2.py
+++ milestone_2.py
@@ -8,3 +8,7 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("User, please input a letter")
+if len(guess) == 1 and guess.isalpha == True:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
