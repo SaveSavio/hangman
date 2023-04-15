25d6b40a5e970456cdf61a09a45fa1427fc2822e
Saverio Tufano <saverio.tufano@gmail.com>
2023-04-15 15:31:59 +0100

    Performs the following project tasks: Task1, Task2, Task3

---
+++ milestone_2.py
@@ -0,0 +1,10 @@
+import random
+# create a list with your 5 top favourite fruits and print it to the screen
+word_list = ["prickly pear", "lemon", "orange", "cherry", "mulberry"]
+print(word_list)
+
+# choose a random element from word_list
+word = random.choice(word_list) 
+print(word)
+
+guess = input("User, please input a letter")
